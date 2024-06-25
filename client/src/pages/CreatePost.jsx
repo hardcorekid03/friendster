f119218efa56dd98a70c6e2305ea7a22b2bbae3c
hardcorekid03 <parasapremiumko88@gmail.com>
2024-06-25 16:11:08 +0800

    .

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,38 +1,38 @@
-import React, { useState } from 'react';
-import { ArrowLeftIcon } from '@heroicons/react/24/outline';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import { Link } from 'react-router-dom';
-import FileUpload from '../components/FileUpload';
-import useTitleAndSlug from '../hooks/useTitleAndSlug';
+import React, { useState } from "react";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { Link } from "react-router-dom";
+import useTitleAndSlug from "../hooks/useTitleAndSlug";
 
 function CreatePost() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const handleChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
   const containerStyle = {
-    height: '600px', // Set your desired height
+    height: "600px", // Set your desired height
   };
 
-  const [blogbody, setBlogbody] = useState('');
-  const [author, setAuthor] = useState('hardcorekid03');
-  const [error, setError] = useState('');
-  const [image, setImage] = useState('');
-  const { title, slug, handleTitleChange, resetTitleAndSlug } = useTitleAndSlug();
-
-  const handleFileSelect = (fileName) => {
-    console.log('Received file name in CreatePost:', fileName);
-    setImage(fileName);
-  };
+  const [blogbody, setBlogbody] = useState("");
+  const [author, setAuthor] = useState("hardcorekid03");
+  const [error, setError] = useState("");
+  const [image, setImage] = useState("");
+  const { title, slug, handleTitleChange, resetTitleAndSlug } =
+    useTitleAndSlug();
 
 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const blog = { image , title, slug, blogbody, author };
-    const response = await fetch('/api/blogs', {
-      method: 'POST',
+    const blog = { image, title, slug, blogbody, author };
+    const response = await fetch("/api/blogs", {
+      method: "POST",
       body: JSON.stringify(blog),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
@@ -42,16 +42,15 @@ function CreatePost() {
     }
     if (response.ok) {
       resetTitleAndSlug();
-      setBlogbody('');
-      setAuthor('');
+      setBlogbody("");
+      setAuthor("");
       setError(null);
-      console.log('Blog posted!');
+      console.log("Blog posted!");
       alert("blog posted!");
     }
   };
-    return (
+  return (
     <div className="items-center justify-center p-2 mb-8">
-
       <div className="flex items-center justify-between p-2 sm:p-2">
         <h3 className="text-xl font-semibold">Create Post</h3>
         <Link
@@ -62,9 +61,30 @@ function CreatePost() {
         </Link>
       </div>
       <form onSubmit={handleSubmit}>
+        <div className="container mx-auto flex  flex-col">
+          <div className="preview-img p-2">
+            <h2>Add Image:</h2>
+            <input type="file" onChange={handleChange} />
+          </div>
+
+          {/* Conditional rendering */}
+          {selectedFile ? (
+            <div className="App mb-4 w-[100%] md:h-[400px] h-[250px] p-2 sm:p-2">
+              <img
+                src={URL.createObjectURL(selectedFile)}
+                alt="Selected File"
+                className="h-full w-full object-cover"
+                style={{ maxWidth: "100%", maxHeight: "400px" }}
+              />
+            </div>
+          ) : (
+            console.log("hello")
+          )}
+        </div>
+
         <div className="preview-img p-2">
-        <FileUpload onFileSelect={handleFileSelect} />
-        <input
+          {/* <FileUpload onFileSelect={handleFileSelect} /> */}
+          <input
             className="w-full border-2 border-gray-300 focus:border-blue-500 focus:outline-none focus:border-opacity-100 px-4 py-2"
             placeholder="Enter blog title"
             value={title}
@@ -103,8 +123,11 @@ function CreatePost() {
             </svg>
             <span>Cancel</span>
           </button>
-          {error && <div className="px-2 py-8 mt-4 border-red-500 border">Error: {error}</div>}
-
+          {error && (
+            <div className="px-2 py-8 mt-4 border-red-500 border">
+              Error: {error}
+            </div>
+          )}
         </div>
       </form>
     </div>
--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -5,48 +5,48 @@ import { Link } from "react-router-dom";
 function PostDetails() {
   return (
     <>
-        <div className="items-center justify-center p-4 sm:p-2 ">
-      <div className="flex items-center justify-between p-4 sm:p-2">
-        <h3 className="text-xl font-semibold ">Post Details</h3>
-        <Link
-          to="/"
-          className="text-xl font-semibold hover:text-gray-700 cursor-pointer h-8 w-8 justify-center"
-        >
-          <ArrowLeftIcon className="h-full w-full" />
-        </Link>
-      </div>
-      <div className="container mx-auto flex  py-4 items-center justify-center flex-col ">
-        <div className=" mb-4 w-[100%] md:h-[400px] h-[250px] p-4 sm:p-2">
-          <img
-            className="h-full w-full object-cover"
-            alt="hero"
-            src="https://www.typingpal.com/en/blog/lorem-ipsum-the-ultimate-placeholder-text/lorem-ipsum@2x.png"
-          />
+      <div className="items-center justify-center p-4 sm:p-2 ">
+        <div className="flex items-center justify-between p-4 sm:p-2">
+          <h3 className="text-xl font-semibold ">Post Details</h3>
+          <Link
+            to="/"
+            className="text-xl font-semibold hover:text-gray-700 cursor-pointer h-8 w-8 justify-center"
+          >
+            <ArrowLeftIcon className="h-full w-full" />
+          </Link>
         </div>
-        <div className="p-4 sm:p-2">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            What is Lorem Ipsum?
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <span className="font-semibold text-blue-700 cursor-pointer">
-            by: @hardcorekid03
-          </span>
-          <div className="flex justify-center"></div>
+        <div className="container mx-auto flex  py-4 items-center justify-center flex-col ">
+          <div className=" mb-4 w-[100%] md:h-[400px] h-[250px] p-4 sm:p-2">
+            <img
+              className="h-full w-full object-cover"
+              alt="hero"
+              src="https://www.typingpal.com/en/blog/lorem-ipsum-the-ultimate-placeholder-text/lorem-ipsum@2x.png"
+            />
+          </div>
+          <div className="p-4 sm:p-2">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              What is Lorem Ipsum?
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <span className="font-semibold text-blue-700 cursor-pointer">
+              by: @hardcorekid03
+            </span>
+            <div className="flex justify-center"></div>
+          </div>
         </div>
       </div>
-    </div>
     </>
-
   );
 }
 
