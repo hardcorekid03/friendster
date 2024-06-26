3949bd4c457e24bc9a2629303dd3d11303bbbd77
hardcorekid03 <parasapremiumko88@gmail.com>
2024-06-27 07:33:58 +0800

    display data to post details

--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { Link, useParams } from "react-router-dom";
-import { IF, URL} from "./url";
-
+import { IF, URL } from "./url";
 
 function PostDetails() {
   const { id } = useParams();
@@ -12,7 +11,7 @@ function PostDetails() {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const response = await fetch(URL +`api/blogs/${id}`);
+        const response = await fetch(URL + `api/blogs/${id}`);
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
@@ -27,7 +26,7 @@ function PostDetails() {
 
     fetchBlogs();
   }, [id]);
-console.log(id);
+  console.log(id);
 
   return (
     <>
@@ -42,30 +41,37 @@ console.log(id);
           </Link>
         </div>
         <div className="container mx-auto flex  py-4 items-center justify-center flex-col ">
-          <div className=" mb-4 w-[100%] md:h-[400px] h-[250px] p-4 sm:p-2">
-            <img
-              className="h-full w-full object-cover"
-              alt="hero"
-              src="https://www.typingpal.com/en/blog/lorem-ipsum-the-ultimate-placeholder-text/lorem-ipsum@2x.png"
-            />
-          </div>
-          <div className="p-4 sm:p-2">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-{blogDetails.title}
-            </h1>
-            <div className="mb-2 leading-relaxed">
-                      <div
-                        dangerouslySetInnerHTML={{
-                          __html:
-                          blogDetails.blogbody,
-                        }}
-                      />
-                    </div>
-            <span className="font-semibold text-blue-700 cursor-pointer">
-              by: @hardcorekid03
-            </span>
-            <div className="flex justify-center"></div>
-          </div>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            blogDetails && (
+              <>
+                <div className=" mb-4 w-[100%] md:h-[400px] h-[250px] p-4 sm:p-2">
+                  <img
+                    className="h-full w-full object-cover"
+                    alt="hero"
+                    src={IF + blogDetails.image}
+                  />
+                </div>
+                <div className="p-4 sm:p-2">
+                  <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                    {blogDetails.title}
+                  </h1>
+                  <div className="mb-2 leading-relaxed">
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: blogDetails.blogbody,
+                      }}
+                    />
+                  </div>
+                  <span className="font-semibold text-blue-700 cursor-pointer">
+                    by: @hardcorekid03
+                  </span>
+                  <div className="flex justify-center"></div>
+                </div>
+              </>
+            )
+          )}
         </div>
       </div>
     </>
