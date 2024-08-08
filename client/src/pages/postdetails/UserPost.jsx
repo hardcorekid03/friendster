fb3a025f6f6b71b72647335b07faab6b96584f72
hardcorekid03 <parasapremiumko88@gmail.com>
2024-08-08 16:21:51 +0800

    add spinner

--- client/src/pages/postdetails/UserPost.jsx
+++ client/src/pages/postdetails/UserPost.jsx
@@ -25,8 +25,15 @@ function UserPost({ loading, data, error, favorites, handleFavorite }) {
   return (
     <div className="UserPost mb-8">
       {loading ? (
-        <div className="container py-5 text-gray-700 dark:bg-spot-dark2 dark:text-spot-light">
-          <p>Loading...</p>
+        <div className="container flex py-5 items-center justify-center dark:text-spot-light">
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-spot-white"
+            role="status"
+          >
+            <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
         </div>
       ) : data.length === 0 ? (
         <div className="container py-5 text-gray-700 dark:bg-spot-dark2 dark:text-spot-light">
