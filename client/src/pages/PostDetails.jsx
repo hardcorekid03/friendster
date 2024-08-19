cd56d8463cbe9412e3ff2810bc64dd9fd70ccdfa
hardcorekid03 <parasapremiumko88@gmail.com>
2024-08-19 16:17:27 +0800

    add spinner on post details

--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -175,7 +175,16 @@ function PostDetails() {
         <div className="bg-white items-center justify-center p-4 mb-8 dark:bg-spot-dark2">
           <div className="container mx-auto flex py-4 flex-col mb-4 ">
             {loading ? (
-              <p className="dark:text-spot-light">Loading...</p>
+              <div className="container flex py-5 items-center justify-center dark:text-spot-light">
+                <div
+                  className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-spot-white"
+                  role="status"
+                >
+                  <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    Loading...
+                  </span>
+                </div>
+              </div>
             ) : (
               blogDetails && (
                 <>
