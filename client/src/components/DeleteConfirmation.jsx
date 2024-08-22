8df3af1d7e8a544e2092e1d90670349969241cf4
hardcorekid03 <parasapremiumko88@gmail.com>
2024-08-22 11:10:16 +0800

    .

--- client/src/components/DeleteConfirmation.jsx
+++ client/src/components/DeleteConfirmation.jsx
@@ -9,11 +9,18 @@ const DeleteConfirmation = ({ onDelete, onCancel }) => {
       <div className="flex flex-col md:flex-row items-center">
         <div className="py-1">
           <svg
-            className="fill-current h-6 w-6 text-red-700 mr-4"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className=" size-6 mr-4 text-red-700 "
           >
-            <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+            />
           </svg>
         </div>
         <div className=" sm:tems-center justify-center">
@@ -25,13 +32,13 @@ const DeleteConfirmation = ({ onDelete, onCancel }) => {
       <div className="mt-2 md:mt-0 flex gap-2">
         <button
           onClick={onDelete}
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-6  "
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-6 rounded "
         >
           Yes
         </button>
         <button
           onClick={onCancel}
-          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 "
+          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-6 rounded"
         >
           No
         </button>
