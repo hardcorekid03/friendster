ff6aa7f2b1cf8acd78fe04430c85287aa37d01a7
hardcorekid03 <parasapremiumko88@gmail.com>
2024-08-22 11:51:59 +0800

    comment image routes

--- server/server.js
+++ server/server.js
@@ -8,8 +8,7 @@ const BlogRoutes = require("./routes/BlogRoutes");
 const UserRoutes = require("./routes/UserRoutes");
 const CommentRoutes = require("./routes/CommentRoutes");
 
-const imageRoutes = require("./routes/ImageRoutes");
-// const userImageRoutes = require("./routes/userImageRoutes");
+// const imageRoutes = require("./routes/ImageRoutes");
 
 const app = express();
 
@@ -29,14 +28,14 @@ app.use(
 app.use(express.json());
 
 // Serve static files from 'assets/images' directory
-app.use("/images", express.static(path.join(__dirname, "assets/images/blogs")));
-app.use(
-  "/banners",
-  express.static(path.join(__dirname, "assets/images/banners"))
-);
-app.use(
-  "/profiles",
-  express.static(path.join(__dirname, "assets/images/profiles"))
+// app.use("/images", express.static(path.join(__dirname, "assets/images/blogs")));
+// app.use(
+//   "/banners",
+//   express.static(path.join(__dirname, "assets/images/banners"))
+// );
+// app.use(
+//   "/profiles",
+//   express.static(path.join(__dirname, "assets/images/profiles"))
 );
 
 app.use((req, res, next) => {
